79b038054862bd5fad3de7d743f1ede51f13ce0f
Igor Tomazelli Nietmann <Igor.Tomazelli.Nietmann@ibm.com>
2021-12-03 14:41:23 -0300

    inicio SpringBoot e Maven

---
+++ exercicioSpringBootJ21ZTH/src/main/java/br/com/cod3r/exercicioSpringBootJ21ZTH/ExercicioSpringBootJ21ZthApplication.java
@@ -0,0 +1,15 @@
+package br.com.cod3r.exercicioSpringBootJ21ZTH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSpringBootJ21ZthApplication {
+
+	public static void main(String[] args) {//Essa classe foi gerada automaticamente no arquivo do start.spring.io
+		SpringApplication.run(ExercicioSpringBootJ21ZthApplication.class, args);
+		//Ao rodar essa classe, o SpringBoot será startado e a porta será indicado no console (padrão 8080)
+		//Ele usa o servidor Tomcat 
+	}
+
+}
---
+++ exercicioSpringBootJ21ZTH/src/test/java/br/com/cod3r/exercicioSpringBootJ21ZTH/ExercicioSpringBootJ21ZthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exercicioSpringBootJ21ZTH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSpringBootJ21ZthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
